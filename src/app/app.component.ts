4668ad5248a8cf427d8a61e903de106e934e6068
pmb5 <pmb5@users.noreply.github.com>
2019-03-01 13:36:41 +0000

    Sample angular7 config page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  public settings = new MySettings();
+
   title = 'config-page';
+
+
+  ngOnInit(): void {
+    let data = (document.querySelector("#JsonSettings") as HTMLInputElement).value;
+
+    if(data !== "")
+      this.settings = JSON.parse(data);
+    else
+      this.settings = new MySettings();
+  }
+
+  public serializeSettings(): void{
+    var input = document.querySelector("#JsonSettings") as HTMLInputElement;
+    input.value = JSON.stringify(this.settings);
+  }
+}
+
+export class MySettings
+{
+  public myStringSetting: string;
+  public myBoolSetting: string;
+  public mySelectSetting: string;
 }
